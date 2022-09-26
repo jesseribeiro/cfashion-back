da4047a6db02affc7a426013aa5a5ddcb06e49c2
Jessé <jesse@trulogic.com.br>
2022-09-26 14:30:58 -0300

    part 1 message

---
+++ src/main/java/br/com/crista/fashion/message/JwtResponse.java
@@ -0,0 +1,33 @@
+package br.com.crista.fashion.message;
+
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+ 
+    public JwtResponse(String accessToken) {
+        this.token = accessToken;
+    }
+ 
+    public String getAccessToken() {
+        return token;
+    }
+ 
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+ 
+    public String getTokenType() {
+        return type;
+    }
+ 
+    public void setTokenType(String tokenType) {
+        this.type = tokenType;
+    }
+}
