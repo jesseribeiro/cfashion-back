3ab7bd9fd72301566e488a3f02ad1990b388d6cd
Jesse Ribeiro <72658685+jesseribeiro@users.noreply.github.com>
2022-09-27 10:00:00 -0300

    Delete src/main/resources directory

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3f4c10234415a5491ba23b590c251e763ea65b416bbd114bef1a597ba62c6645
-size 1707001
--- src/main/resources/sqls-insert.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ff89308bb53c5519a7ddd5e990bf26f69186d975905b25d4356761db4e3390f6
-size 1005154
