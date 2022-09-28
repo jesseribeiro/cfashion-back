7d918fd61bfc770662f0048a0faa94075d4d53c7
Jessé <jesse@trulogic.com.br>
2022-09-28 02:40:21 -0300

    part 6

--- src/main/java/br/com/crista/fashion/config/CentralConfig.java
+++ src/main/java/br/com/crista/fashion/config/CentralConfig.java
@@ -3,34 +3,9 @@ package br.com.crista.fashion.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-
 @Component
 public class CentralConfig {
 
     @Value("${central.path}")
     private String centralUrl;
-
-    public String getCentralPath(){
-        return centralUrl;
-    }
-
-    public String getCentralImagesPath() {
-        return centralUrl.concat("/images/");
-    }
-
-    public String getCentralAnexosPath() {
-        return centralUrl.concat("/anexos/");
-    }
-
-    public String getCentralRemessasPath() {
-        return centralUrl.concat("/remessas/");
-    }
-
-    public String getCentralTempPath() {
-        return centralUrl.concat("/temp/");
-    }
-    public String getCentralImportacaoPath() {
-        return centralUrl.concat("/importacao/");
-    }
 }
